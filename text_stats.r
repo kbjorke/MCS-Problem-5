57aaca7e7ef436bdd77aeec5d78e1c573c83ec38
Kristian Bjørke <kbjorke@gmail.com>
2015-03-26 17:34:59 +0000

    Included a method to get the character probability for the text. This creates a .txt file with the probabilities and a plot of the probabilities. The probabilities I got corresponded to what i found online for letter probabilities for english.

--- text_stats.r
+++ text_stats.r
@@ -3,10 +3,10 @@
 args = commandArgs(trailingOnly = TRUE)
 
 filename = args[1]
+#filename = "test_pg2600.txt"
 
 # Read the text file of reference textr:
 reference_text_original = readLines(filename)
-#reference_text_original = readLines("pg2600.txt")
 
 # Combine into one continous vector:
 reference_text = paste(reference_text_original, collapse=" ")
@@ -57,11 +57,18 @@ transprob_matrix = matrix(0, nrow=length(character_list),
                           ncol=length(character_list),
                           dimnames=list(character_list, character_list))
 
+# Create an empty vector to be used for character probabilities:
+char_prob = array(data=0, dim=length(character_list),
+                  dimnames=list(character_list))
+
+
+
+num_char = length(reference_text)
 
 # Initialization of loop:
 current_char = reference_text[1]
 
-num_char = length(reference_text)
+char_prob[current_char] = char_prob[current_char] + 1
 
 # Loop over characters in reference text:
 for( i in 2:num_char )
@@ -74,14 +81,18 @@ for( i in 2:num_char )
 
     next_char = reference_text[i]
 
+    # Contribution to transition probability matrix:
     transprob_matrix[current_char, next_char] = 
                         transprob_matrix[current_char, next_char] + 1
 
     current_char = next_char
+    
+    # Contribution to characeter probabilities:
+    char_prob[current_char] = char_prob[current_char] + 1
 }
 
 
-# Normalization loop (normalize each row):
+# Normalization loop (normalize each row) for transition probability matrix:
 for( char in character_list )
 {
     norm_factor = sum(transprob_matrix[char, ])
@@ -91,6 +102,9 @@ for( char in character_list )
     }
 }
 
+# Normalize character probabilities:
+char_prob = char_prob/num_char
+
 
 # Write the content of the transition matrix to a .txt file:
 write.table(transprob_matrix, 
@@ -99,19 +113,34 @@ write.table(transprob_matrix,
 
 
 # Read out transition matrix, to be used in encryption script:
-#transprob_matrix_readout = read.table("matrix.txt", header=TRUE)
+#transprob_matrix_ = read.table("matrix.txt", header=TRUE)
 #transprob_matrix_ = data.matrix(transprob_matrix)
 
+write.table(char_prob,
+            file=sprintf("char_prob-%s-.txt", 
+                         gsub(".txt", "", filename)),
+            col.names=FALSE)
 
-# Create .eps figure of transition probability matrix:
+# Read out character probabilities, to be used in encryption script:
+#char_prob_ = read.table(sprintf("char_prob-%s-.txt", 
+#                                gsub(".txt", "", filename)))
+#char_prob = data.matrix(char_prob)
+
+
 setEPS()
+# Create .eps figure of transition probability matrix:
 postscript(sprintf("transprob_matrix-%s-.eps", gsub(".txt", "", filename)))
 
 library(lattice)
 levelplot(t(apply(transprob_matrix, 2, rev)), 
           col.regions = gray(1-0:255/255),
-          main=sprintf('Transition probablity matrix for "%s"', 
-                       filename),
+          main=sprintf('Transition probablity matrix for "%s"', filename),
           xlab='Probability of Second Letter', 
           ylab='Condition on First Letter')
 
+# Create .eps figure of char probabilities:
+postscript(sprintf("char_prob-%s-.eps", gsub(".txt", "", filename)))
+
+barplot(char_prob, las=2,
+        xlab="Characters", ylab="Probability (relative frequency)",
+        main=sprintf('Character probabilities for "%s"', filename))

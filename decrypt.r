e0ae8e4db0ec1ba0ecc2ee421eb3257dc3ccf576
Kristian Bjørke <kbjorke@gmail.com>
2015-03-28 15:50:31 +0000

    Have made the Monte Carlo loop. Not giving decrypted results. Need to work more on it to get it right and maybe also find a way to speed it up.

--- decrypt.r
+++ decrypt.r
@@ -1,71 +1,33 @@
 # By: Kristian Bjoerke
 
+source("code_format.r")
+
 #args = commandArgs(trailingOnly = TRUE)
 
 #filename = args[1]
 filename = "example_encrypted_text.txt"
 
+# Number of Monte Carlo samples:
+N = 10000
+
 text_stats_source = "pg2600.txt"
 
 encrypted_text = readLines(filename)
 
-### Make includable function of given section, for use in both programs ###
+output = code_format(encrypted_text)
 
-# Combine into one continous vector:
-formated_encrypted_text = paste(encrypted_text, collapse=" ")
-
-# Change all lower case characters to upper case characters: 
-formated_encrypted_text = toupper(formated_encrypted_text)
-
-# Replace all punctuation characters with white space:
-formated_encrypted_text = gsub("[[:punct:]]", " ", formated_encrypted_text)
-
-# Remove all digits:
-formated_encrypted_text = gsub("[[:digit:]]", "", formated_encrypted_text)
-
-# Create list of special characters and A-Z replacements:
-special_characters = list('Š'='S', 'š'='s', 'Ž'='Z', 'ž'='z', 'À'='A', 
-                          'Á'='A', 'Â'='A', 'Ã'='A', 'Ä'='A', 'Å'='A', 
-                          'Æ'='A', 'Ç'='C', 'È'='E', 'É'='E', 'Ê'='E', 
-                          'Ë'='E', 'Ì'='I', 'Í'='I', 'Î'='I', 'Ï'='I', 
-                          'Ñ'='N', 'Ò'='O', 'Ó'='O', 'Ô'='O', 'Õ'='O', 
-                          'Ö'='O', 'Ø'='O', 'Ù'='U', 'Ú'='U', 'Û'='U', 
-                          'Ü'='U', 'Ý'='Y', 'Þ'='B', 'ß'='Ss', 'à'='a', 
-                          'á'='a', 'â'='a', 'ã'='a', 'ä'='a', 'å'='a', 
-                          'æ'='a', 'ç'='c', 'è'='e', 'é'='e', 'ê'='e', 
-                          'ë'='e', 'ì'='i', 'í'='i', 'î'='i', 'ï'='i', 
-                          'ð'='o', 'ñ'='n', 'ò'='o', 'ó'='o', 'ô'='o', 
-                          'õ'='o', 'ö'='o', 'ø'='o', 'ù'='u', 'ú'='u', 
-                          'û'='u', 'ý'='y', 'ý'='y', 'þ'='b', 'ÿ'='y')
-
-# Repleace special characters with A-Z/a-z characters:
-formated_encrypted_text = chartr(paste(names(special_characters), collapse=''),
-                        paste(special_characters, collapse=''),
-                        formated_encrypted_text)
-
-# Remove all unnecessary white spaces:
-formated_encrypted_text = gsub("[[:space:]]+", " ", formated_encrypted_text)
-
-# Split the list into an array of the characters:
-# (Makes the loop faster, because not using substr() function)
-formated_encrypted_text = strsplit(formated_encrypted_text, '')[[1]]
-
-
-# Create an list of the character pluss emtpty space:
-character_list = c(' ', toupper(letters))
-
-#########
-
-
+formated_encrypted_text = output[[1]]
+character_list = output[[2]]
 
 # Read out transition matrix, to be used in encryption script:
 transprob_matrix = read.table(sprintf("transprob_matrix-%s-.txt", 
                                       gsub(".txt", "", text_stats_source)))
 
 transprob_matrix = data.matrix(transprob_matrix)
-#transprob_matrix = matrix(transprob_matrix, nrow=length(character_list),
-#                          ncol=length(character_list),
-#                          dimnames=list(character_list, character_list))
+transprob_matrix = matrix(transprob_matrix, nrow=length(character_list),
+                          ncol=length(character_list),
+                          dimnames=list(character_list, character_list))
+
 
 # Read out character probabilities, to be used in encryption script:
 char_prob = read.table(sprintf("char_prob-%s-.txt",
@@ -110,8 +72,6 @@ sorted_char_freq_enctext = dimnames(sort(initial_rel_char_freq,
 initial_key = array(data=NA, dim=length(character_list),
                     dimnames=list(character_list))
 
-print(sorted_char_freq_source)
-print(sorted_char_freq_enctext)
 
 for( i in 1:length(character_list) ) 
 {
@@ -119,11 +79,72 @@ for( i in 1:length(character_list) )
                                     sorted_char_freq_source[[1]][i]
 }
 
+code_key = initial_key
+
+score_function = function(formated_text, key, transprob_mat)
+{
+    score_func = 0
+    for( i in 1:(num_char-1) )
+    {
+        first_character = key[formated_text[i]]
+        second_character = key[formated_text[i+1]] 
+
+        score_func = score_func + transprob_mat[first_character,
+                                                second_character]
+    }
+
+    return(score_func)
+}
+
+
+score_func = score_function(formated_encrypted_text, code_key, transprob_matrix)
+
+
+for( i in 1:N )
+{
+    char_swap = sample(character_list,2)
+
+    new_code_key = chartr(paste(char_swap, collapse=''), 
+                          paste(rev(char_swap), collapse=''), 
+                          initial_key)
+
+    new_score_func = score_function(formated_encrypted_text, new_code_key,
+                                    transprob_matrix)
+
+    if( new_score_func >= score_func )
+    {
+        code_key = new_code_key
+        score_func = new_score_func
+    }
+    else
+    {
+        alpha = new_score_func/score_func
+
+        r = runif(1)
+
+        if( alpha >= r )
+        {
+            code_key = new_code_key
+            score_func = new_score_func
+        }
+    }
+    #print(score_func)
+}
+
+
 decrypted_text = array(data=NA, dim=length(encrypted_text))
 
+#print(encrypted_text)
 for( i in 1:length(encrypted_text) )
 {
-    print(encrypted_text[i])
+    # Alternative way of solving, may be slower/faster
+#    decrypted_text[i] = gsub(sorted_char_freq_enctext[[1]],
+#                          sorted_char_freq_source[[1]], encrypted_text[i])
+    decrypted_text[i] = chartr(paste(names(code_key), collapse=''),
+                        paste(code_key, collapse=''),
+                        encrypted_text[i])
 }
+print("")
+print(decrypted_text)
 
 
